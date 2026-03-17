ce548736153884701742efcf19300350b4cba2fb
Rio Weber <riodweber@gmail.com>
2026-03-17 14:54:39 -0400

    update deploy;

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ cd /var/www/compare.django
 git pull origin master
 source .venv/bin/activate
 uv sync
-python manage.py collectstatic --noinput
+PRODUCTION=true python manage.py collectstatic --noinput
 sudo cp /var/www/compare.django/gunicorn.service /etc/systemd/system/gunicorn.service
 sudo systemctl daemon-reload
 sudo systemctl enable gunicorn
